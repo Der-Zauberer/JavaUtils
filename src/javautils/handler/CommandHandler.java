90c13aaf5b3c3863fc3c163d6f35b5814e6e06bb
Der-Zauberer <and.som282@gmail.com>
2021-10-09 16:07:52 +0200

    Updated CommandHandler and Console

--- src/javautils/handler/CommandHandler.java
+++ src/javautils/handler/CommandHandler.java
@@ -6,14 +6,14 @@ import java.util.HashMap;
 import javautils.util.Command;
 
 public class CommandHandler {
-	
+
 	private static HashMap<String, Command> commands = new HashMap<>();
 	private static ArrayList<String> history = new ArrayList<>();
-	
+
 	public static void registerCommand(String label, Command command) {
 		commands.put(label, command);
 	}
-	
+
 	public static boolean executeCommand(String string) {
 		history.add(string);
 		String command[] = getSplitedCommand(string);
@@ -21,11 +21,11 @@ public class CommandHandler {
 		String args[] = Arrays.copyOfRange(command, 1, command.length);
 		return executeCommand(label, args);
 	}
-	
-	public static boolean executeCommand(String label, String args[]) {
-		for(String string : commands.keySet()) {
-			if(string.equalsIgnoreCase(label)) {
-				if(args.length > 0 && args[0].equalsIgnoreCase("help")) {
+
+	private static boolean executeCommand(String label, String args[]) {
+		for (String string : commands.keySet()) {
+			if (string.equalsIgnoreCase(label)) {
+				if (args.length > 0 && args[0].equalsIgnoreCase("help")) {
 					sendMessageOutput(commands.get(string).getCommandHelp());
 					return true;
 				}
@@ -34,89 +34,79 @@ public class CommandHandler {
 		}
 		return false;
 	}
-	
+
 	public static HashMap<String, Command> getCommands() {
 		return commands;
 	}
-	
+
 	public static Command getCommand(String string) {
-		if(commands.containsKey(string)) {
+		if (commands.containsKey(string)) {
 			return commands.get(string);
 		}
 		return null;
 	}
-	
+
 	public static boolean getCondition(String args[], String condition, int position) {
-		if(args.length - 1 >= position && args[position] != null && args[position].equalsIgnoreCase(condition)) return true;
+		if (args.length - 1 >= position && args[position] != null && args[position].equalsIgnoreCase(condition))
+			return true;
 		return false;
 	}
-	
+
 	public static boolean hasCondition(String args[], String condition) {
 		for (int i = 0; i < args.length; i++) {
-			if(args[i].equalsIgnoreCase(condition)) return true;
+			if (args[i].equalsIgnoreCase(condition))
+				return true;
 		}
 		return false;
 	}
-	
+
 	public static ArrayList<String> getHistory() {
 		return history;
 	}
-	
+
 	public static void sendMessageOutput(Object object) {
 		System.out.println(object);
 	}
-	
+
 	public static void sendInfoOutput(Object object) {
 		System.out.println("[INFO] " + object);
 	}
-	
+
 	public static void sendWarningOutput(Object object) {
 		System.out.println("[WARNING] " + object);
 	}
-	
+
 	public static void sendErrorOutput(Object object) {
 		System.out.println("[ERROR] " + object);
 	}
-	
+
 	private static String[] getSplitedCommand(String string) {
-		if(string == null || string.isBlank()) {
-			String list[] = {""};
-			return list;
-		} else if(string.split(" ").length == 1){
-			String list[] = {string};
+		if (string == null || string.isBlank()) {
+			String list[] = { "" };
 			return list;
 		}
 		ArrayList<String> strings = new ArrayList<>();
-		boolean enclosedstring = false;
-		int lastspace = 0;
+		string += " ";
+		int lastspace = -1;
+		char lastchar = ' ';
+		char character = ' ';
+		boolean enclosed = false;
 		for (int i = 0; i < string.length(); i++) {
-			if(string.charAt(i) == '"') {
-				if(!enclosedstring) {
-					enclosedstring = true;
-				} else {
-					enclosedstring = false;
+			character = string.charAt(i);
+			if (character == ' ' && !enclosed) {
+				if (lastchar != ' ' && lastchar != '"') {
+					strings.add(string.substring(lastspace + 1, i));
 				}
+				lastspace = i;
+			} else if (character == '"' && !enclosed) {
+				lastspace = i;
+				enclosed = true;
+			} else if (character == '"' && enclosed) {
+				strings.add(string.substring(lastspace + 1, i));
+				lastspace = i;
+				enclosed = false;
 			}
-			if(string.charAt(i) == ' ' || i == string.length() - 1) {
-				if(!enclosedstring) {
-					String substring;
-					if(lastspace == 0) {
-						substring = string.substring(0, i);
-					} else if(i == string.length() - 1 && string.charAt(i) != ' ') {
-						substring = string.substring(lastspace + 1, i + 1);
-					} else  {
-						substring = string.substring(lastspace + 1, i);
-					}
-					if(!substring.isBlank()) {
-						if(substring.startsWith("\"") && substring.endsWith("\"")) {
-							substring = substring.substring(1, substring.length() - 1);
-						}
-						strings.add(substring);
-					}
-					lastspace = i;
-				}
-				
-			}
+			lastchar = character;
 		}
 		String command[] = new String[strings.size()];
 		for (int i = 0; i < command.length; i++) {
--- src/javautils/util/Console.java
+++ src/javautils/util/Console.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 import javautils.handler.CommandHandler;
 
 public class Console {
-	
+
 	private static Thread thread;
-	private static String prefix = "";
+	private static String prefix;
+
+	public static void startConsole() {
+		startConsole("");
+	}
 	
 	@SuppressWarnings("resource")
-	public Console() {
+	public static void startConsole(String prefix) {
+		Console.prefix = prefix;
 		thread = new Thread(() -> {
 			Scanner scanner = new Scanner(System.in);
 			while (true) {
@@ -19,19 +24,15 @@ public class Console {
 		});
 		thread.start();
 	}
-	
+
 	public void stopConsole() {
 		thread.interrupt();
 	}
-	
-	public void startConsole() {
-		thread.start();
-	}
-	
+
 	public static void setPrefix(String prefix) {
 		Console.prefix = prefix;
 	}
-	
+
 	public static String getPrefix() {
 		return prefix;
 	}
