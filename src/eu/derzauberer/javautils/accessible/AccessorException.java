03943aa981183d7d4fe01ff2a939097026bbf7cf
Der-Zauberer <and.som282@gmail.com>
2022-10-31 12:59:29 +0100

    Added documentation for the AccessorException

--- src/eu/derzauberer/javautils/accessible/AccessorException.java
+++ src/eu/derzauberer/javautils/accessible/AccessorException.java
@@ -1,9 +1,21 @@
 package eu.derzauberer.javautils.accessible;
 
+/**
+ * A exception, which is thrown, when another exception was thrown inside the
+ * functions of the {@link Accessor}, {@link FieldAccessor} or
+ * {@link MethodAccessor}.
+ */
 public class AccessorException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Creates a new exception, which is typically thrown, when another exception
+	 * was thrown inside the functions of the {@link Accessor},
+	 * {@link FieldAccessor} or {@link MethodAccessor}.
+	 * 
+	 * @param string the exception message
+	 */
 	public AccessorException(String string) {
 		super(string);
 	}
