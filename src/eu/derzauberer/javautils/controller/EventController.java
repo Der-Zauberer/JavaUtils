b1a51ee662b3ca0b08ce5f5d15c588df42b64c42
Der-Zauberer <and.som282@gmail.com>
2022-11-03 10:47:47 +0100

    Added EventController

---
+++ src/eu/derzauberer/javautils/controller/EventController.java
@@ -0,0 +1,68 @@
+package eu.derzauberer.javautils.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import eu.derzauberer.javautils.events.Event;
+import eu.derzauberer.javautils.util.EventListener;
+
+/**
+ * The controller deals with incoming events and calls the corresponding
+ * {@link EventListener} to do specific things when called.
+ */
+public class EventController {
+	
+	private final List<EventListener<?>> listeners = new ArrayList<>();
+	private static final EventController globalEventController = new EventController();
+	
+	/**
+	 * Adds a listener to the controller. The listener will be called when a event
+	 * occurs.
+	 * 
+	 * @param listener the listener to add
+	 */
+	public void addListener(EventListener<?> listener) {
+		listeners.add(listener);
+		Collections.sort(listeners);
+	}
+
+	/**
+	 * Removes a listener form the controller.
+	 * 
+	 * @param listener the listener to remove
+	 */
+	public void removeListener(EventListener<?> listener) {
+		listeners.remove(listener);
+	}
+	
+	/**
+	 * Returns an unmodifiable list of all added events of the controller. The
+	 * events are sorted by their priority.
+	 * 
+	 * @return an unmodifiable list of events
+	 */
+	public List<EventListener<?>> getListeners() {
+		return Collections.unmodifiableList(listeners);
+	}
+	
+	/**
+	 * Call all listeners, which are instances of the event.
+	 * 
+	 * @param event the event, that occurs
+	 */
+	public void callListeners(Event event) {
+		listeners.stream()
+			.filter(listener -> listener.getEventType().isAssignableFrom(event.getClass()))
+			.forEach(listener -> listener.acceptUnchecked(event));
+	}
+	
+	/**
+	 * This is the global event controller, which handles all events that occur.
+	 * 
+	 * @return the global event controller that handles all events
+	 */
+	public static EventController getGlobaleventcontroller() {
+		return globalEventController;
+	}
+
+}
---
+++ src/eu/derzauberer/javautils/util/EventListener.java
@@ -0,0 +1,106 @@
+package eu.derzauberer.javautils.util;
+
+import java.util.function.Consumer;
+
+import eu.derzauberer.javautils.controller.EventController;
+import eu.derzauberer.javautils.events.Event;
+
+/**
+ * When registered in an {@link EventController}, the listener waits for
+ * incoming events and executes the action {@link Consumer}.
+ *
+ * @param <T> the command, on which occurrence the action executes
+ */
+public class EventListener<T extends Event> implements Comparable<EventListener<?>> {
+	
+	/**
+	 * Represents the priority of events.
+	 */
+	public enum EventPriority {
+
+		EARLIEST(0), 
+		EARLY(1), 
+		NORMAL(2),
+		LATE(3),
+		LATEST(4);
+		
+		private final int priority;
+		
+		private EventPriority(final int priority) {
+			this.priority = priority;
+		}
+		
+		public int getPriority() {
+			return priority;
+		}
+		
+	}
+	
+	private final EventPriority priority;
+	private final Class<T> eventType;
+	private final Consumer<T> action;
+	
+	/**
+	 * Creates a new listener, which when registered in the {@link EventController},
+	 * waits for incoming events and executes the action {@link Consumer}.
+	 * 
+	 * @param priority  when the action should be executed
+	 * @param eventType the class of the event to filter the {@link EventController}
+	 * @param action    the consumer that executes when an event occurs
+	 */
+	public EventListener(EventPriority priority, Class<T> eventType, Consumer<T> action) {
+		this.priority = priority;
+		this.eventType = eventType;
+		this.action = action;
+	}
+	
+	/**
+	 * Returns the priority of the event. The priority defines when the action
+	 * should be executed
+	 * 
+	 * @return the priority of the event
+	 */
+	public EventPriority getPriority() {
+		return priority;
+	}
+	
+	/**
+	 * Returns the class of the event to filter the {@link EventController}.
+	 * 
+	 * @return the class of the event
+	 */
+	public Class<T> getEventType() {
+		return eventType;
+	}
+	
+	/**
+	 * Returns the consumer that executes when an event occurs.
+	 * 
+	 * @return the consumer that executes when an event occurs
+	 */
+	public Consumer<T> getAction() {
+		return action;
+	}
+	
+	/**
+	 * Executes the consumer based on an event.
+	 * 
+	 * @param event the event that occurred
+	 * @throws ClassCastException if the given event is not an instance of the
+	 *                            event, which was used for the
+	 *                            {@link EventListener}
+	 */
+	@SuppressWarnings("unchecked")
+	public void acceptUnchecked(Event event) {
+		action.accept((T) event);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int compareTo(EventListener<?> listener) {
+		return priority.getPriority() - listener.getPriority().getPriority();
+	}
+
+}
