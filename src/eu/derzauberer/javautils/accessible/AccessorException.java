985a89ee0d9e6d5c7b2d0b150744768e9239ca7b
Der-Zauberer <and.som282@gmail.com>
2023-01-09 12:35:28 +0100

    Improved documentation

--- src/eu/derzauberer/javautils/accessible/AccessorException.java
+++ src/eu/derzauberer/javautils/accessible/AccessorException.java
@@ -1,18 +1,18 @@
 package eu.derzauberer.javautils.accessible;
 
 /**
- * An exception, which is thrown, when another exception was thrown inside the
+ * An exception which is thrown when another exception was thrown inside the
  * functions of the {@link Accessor}, {@link FieldAccessor} or
  * {@link MethodAccessor}.
  */
 public class AccessorException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
-	 * Creates a new exception, which is typically thrown, when another exception
-	 * was thrown inside the functions of the {@link Accessor},
-	 * {@link FieldAccessor} or {@link MethodAccessor}.
+	 * Creates a new exception which is typically thrown, when another exception was
+	 * thrown inside the functions of the {@link Accessor}, {@link FieldAccessor} or
+	 * {@link MethodAccessor}.
 	 * 
 	 * @param message the exception message
 	 */
@@ -21,12 +21,12 @@ public class AccessorException extends RuntimeException {
 	}
 
 	/**
-	 * Creates a new exception, which is typically thrown, when another exception
-	 * was thrown inside the functions of the {@link Accessor},
-	 * {@link FieldAccessor} or {@link MethodAccessor}.
+	 * Creates a new exception which is typically thrown, when another exception was
+	 * thrown inside the functions of the {@link Accessor}, {@link FieldAccessor} or
+	 * {@link MethodAccessor}.
 	 * 
 	 * @param message the exception message
-	 * @param cause the exception cause
+	 * @param cause   the exception cause
 	 */
 	public AccessorException(String message, Exception cause) {
 		super(message, cause);
